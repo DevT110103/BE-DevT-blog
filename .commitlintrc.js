1bca01f6d2c0cb08aed9f300ad5b3a1f78f7268d
TaiLe1101 <taic21a.th1@gmail.com>
2023-02-18 20:59:48 +0700

    test commit

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['update', 'add']],
+    'type-enum': [2, 'always', ['update', 'add', 'fix', 'feat']],
   },
 };
